700cd537bb345c053c8052daee3d6e28eee0ae97
Jing Xia <jingxia1219@gmail.com>
2022-02-10 10:05:20 -0800

    yearSelected working

--- src/components/Expenses/Items.js
+++ src/components/Expenses/Items.js
@@ -3,19 +3,23 @@ import '../../UI/Items.css'
 import Card from './Card'
 import ExpensesFilter from './ExpensesFilter';
 import React,{useState} from 'react';
-
+// let expenses= props.expenses;
+  // const [filteredExpenses,setFilteredExpenses] = useState(expenses);
+   // const filtered = expenses.filter(expense => 
+    //   expense.date.getFullYear().toString() === selectedYear.toString()
+    // )
 function Items(props) {
-  const [filteredYear,setFilteredYear] = useState('2022');
-  const filterChangeHandler = (filteredYear) => {
-    setFilteredYear(filteredYear)}
-    
-  // console.log("line17",filteredYear);
+  const [filteredYear,setFilteredYear] = useState('2020');
+  const filterChangeHandler = (selectedYear) => {
+   
+    setFilteredYear(selectedYear)
+  }
   const filteredItems = props.expenses.filter( item => 
     item.date.getFullYear().toString() === filteredYear)
     return (
       <div>
         <Card className='expenses'>
-        <ExpensesFilter selected ={filteredYear}  onChangeFilter={filterChangeHandler}/>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
      {filteredItems.map( expense =>
          <ExpenseItem 
             key={expense.id}
